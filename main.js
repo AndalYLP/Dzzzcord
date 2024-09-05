47ed4c9edaf09f01c8d9dc384468c1fbf2d43e1a
AndalYLP <andal089.az@gmail.com>
2024-09-05 15:12:44 -0500

    test

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ const server = https.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.end(`
     <html>
-      <head>
-        <title>Hostia chavales</title>
-      </head>
       <body style="margin: 0; padding: 0;">
       </body>
     </html>`);
