5badbcbf89e6006bdc54ff67f022558f5bc04f3d
AndalYLP <andal089.az@gmail.com>
2024-09-05 15:30:11 -0500

    fixed variable

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 4000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,7 +19,7 @@ app.get('/script.js', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
